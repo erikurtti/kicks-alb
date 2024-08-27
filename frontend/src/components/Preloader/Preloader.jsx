319644b714f4c61343d6ef17eee78f63f579b928
erikurtti <erjon.kurti@fshnstudent.info>
2024-08-27 17:30:57 +0200

    Update Preloader.jsx

--- frontend/src/components/Preloader/Preloader.jsx
+++ frontend/src/components/Preloader/Preloader.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import './Preloader.css';
 
 const Preloader = () => {
-    const { t } = useTranslation();
 
     return (
         <div className='preloader'>
             <div className='logo-container-preloader'>
-                <div className="loader">{t('loading', 'Loading...')}</div>
+                <div className="loader">Loading...</div>
             </div>
         </div>
     );
